c2ad41b08534fa7ac202ceffe7fadd0efd8e993b
yacoub1204-ui <yacoub1204@gmail.com>
2026-03-10 15:26:39 +0100

    c pas de la fonderie

---
+++ Projet informatique/fournisseur.py
@@ -0,0 +1,23 @@
+import materiau
+
+class fournisseur:
+
+    def __init__(self,  numero: int, x :float, y: float, prix: float ):
+        self.numero = numero
+        self.x = x 
+        self.y = y
+        self.prix = prix 
+   """ def get_z(self):
+        '''épaisseur demandée par le client'''
+        return self._z
+    
+    def get_dict_quantites(self):
+        '''dictionnaire contenant les %ages min et max de matériau voulu par le client'''
+        return self._dict_quantites
+    
+    def get_pmax(self, dict_quantites, mat : Materiau):
+        return dict_quantites[mat][0]
+    
+    def get_pmax(self, dict_quantites, mat : Materiau):
+        return dict_quantites[mat][1]"""
+    
