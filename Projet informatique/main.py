82e0bfd11b26a8050d5ca79e0b50833efdffb70e
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-16 08:15:01 +0100

    Update main.py
    j'ai ajouté le code "main" qui vient de caseine

--- Projet informatique/main.py
+++ Projet informatique/main.py
@@ -14,5 +14,39 @@ def read_file(file : str):
     else:
         raise(ValueError)
 
+
+"""
+Fichier à completer
+Seule la méthode resoudre() sera appelee
+"""
+import os
+import time
+
+
+def resoudre(inst: str):
+    """
+    Cette methode est la seule qui sera appelee par le checker
+
+    :param inst: nom de l'instance traitee
+    :return: rien
+    """
+    debut: float = time.time()
+    # TODO
+    duree: float = time.time() - debut
+    print(f"Durée d'execution : {round(100 * duree) / 100} secondes")
+
+
+if __name__ == "__main__":
+    instance = "nom_instance"
+    time_max = 60
+    if os.path.exists("CONFIG"):
+        with open("CONFIG", "r") as fichier_config:
+            for ligne in fichier_config.readlines():
+                if '=' not in ligne:
+                    continue
+                mots = [_.strip() for _ in ligne.split("=")]
+                if mots[0].upper() == "INSTANCE":
+                    instance = mots[1]
+    resoudre(instance)
     
         
