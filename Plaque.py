150baba10184606b9d2ff5295de617c9daf0d19a
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-28 18:59:39 +0100

    Update Plaque.py

--- Plaque.py
+++ Plaque.py
@@ -2,9 +2,18 @@ import Polygone
 
 class Plaque:
 
-    def __init__(self, prix_d : float, dict_p : dict[Polygone]):
-        self._prix_d = prix_d
+    def __init__(self, x: float, y: float, dict_p: dict[Polygone], prix_d: float):
+        self._x=x
+        self.y=y
+        self._prix_d = prix_d """euro/cm^2"""
         self._dict_p = dict_p
+
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
 
     def get_prix_d(self):
         return self._prix_d
@@ -12,3 +21,19 @@ class Plaque:
     def get_dict_p(self):
         return self._dict_p
 
+    def surface(self):
+        return self._x*self._y
+
+
+    def rotation(self):
+        return Plaque(self._y,self._x,self._dict_p,self._prix_d)
+
+
+
+    def rentre_dedans(self, tole_x,tole_y)
+    """renvoie booléen"""
+    return self.x<=tole_x and self._y <= tole_y
+
+
+    def cout_decoupe(self):
+        return self._prix_d*self.surface()
