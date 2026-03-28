faea2703f806e81aae96bab71e3a97b4c334c03a
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-28 20:11:26 +0100

    Update Fonderie.py

--- Fonderie.py
+++ Fonderie.py
@@ -35,26 +35,61 @@ class Fonderie:
         return self._client
 
 
-    def masse_necessaire(self, i:int):
-        """i c l'indice de la tole a produire"""
-        tole = self._tole[i]
-        alliage = tole.get_all()
-        volume_cm3 = tole.get_x() * tole.get_y() * tole.get_z()
-        #g/cm^3convertit en kg
-        mv_g_cm3 = alliage.get_mv()
-        # mv liste de mv par matériaux
-            pct = alliage.get_list_pct()
-            mv_moy = sum(mv_g_cm3[j] * pct[j] for j in range(len(pct)))
-        else:
-            mv_moy = mv_g_cm3
-        masse_kg = volume_cm3 * mv_moy / 1000.0
-        return masse_kg
-
-    def cout_production_tole(self, i:int):
-        """meme i que masse_necessaire"""
-        tole = self._tole[i]
-        volume_cm3 = tole.get_x() * tole.get_y() * tole.get_z()
-        return volume_cm3 * (tole.get_all().prix_cm3() + self._cout)
+    def pct_triviaux(self) -> list[float]:
+        """ 1.  metaux avec min = max  prioritare
+          2. Tous les autres metaux partent de leur minimum.
+          3. Le reliquat (1 - somme actuelle) est distribué sur les metaux libres (min < max),
+             en remplissant chacun jusqu'a son maximum jusqu'a epuisement du reliquat.
+        :return: liste de proportions (une par metal), somme = 1"""
+        list_min = self._client.get_list_min()
+        list_max = self._client.get_list_max()
+        n = len(list_min)
+ 
+        # Etape 1 : tous les metaux partent de leur minimum
+        pct = list_min[:]
+ 
+        # Etape 2 : reperer les metaux libres (min < max), les fixes sont deja corrects
+        indices_libres = [i for i in range(n) if list_min[i] < list_max[i]]
+ 
+        # Etape 3 : calculer le reliquat a distribuer sur les metaux libres
+        reliquat = 1.0 - sum(pct)
+ 
+        for i in indices_libres:
+            absorbable = list_max[i] - pct[i]   # marge disponible pour ce metal
+            ajout = min(reliquat, absorbable)
+            pct[i] += ajout
+            reliquat -= ajout
+            if reliquat <= 1e-9:                 # reliquat nul (tolerance flottant)
+                break
+ 
+        assert reliquat <= 1e-9, \
+            f"Impossible de satisfaire le cahier des charges (reliquat = {reliquat})"
+ 
+        return pct
+        
+    def mv_alliage(self,pct):
+        inverse_mv = sum(pct[i] / self._mv_metaux[i] for i in range(len(pct)))
+        return 1/ inverse_mv
+        
+    def cout trivial(self, nb_toles):
+        pct=self.pct_triviaux()
+        volume_total_cm3 = nb_toles * self._x * self._y * self._client.get_z()
+ 
+        # Masse totale de metal necessaire (g -> kg)
+        mv = self.mv_alliage(pct)
+        masse_totale_kg = volume_total_cm3 * mv / 1000.0
+ 
+        # Repartition par metal et cout d'achat
+        kg_par_metal = [masse_totale_kg * pct[i] for i in range(len(pct))]
+        cout_achat = sum(kg_par_metal[i] * self._prix_metaux[i] for i in range(len(pct)))
+ 
+        # Cout de fonte et laminage
+        cout_fonte = volume_total_cm3 * self._cout_cm3
+ 
+        cout_total = cout_achat + cout_fonte
+        return cout_total, pct, kg_par_metal
+    
+    
  
         
     
