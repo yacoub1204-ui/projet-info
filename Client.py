3440ece99aa11d151afd3ce206b4f8a259c4dc6f
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-28 18:48:59 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -11,3 +11,10 @@ class Client:
     def get_list_min(self):
         '''pourcentages min de chaque matériau'''
         return self._list_min
+
+    def composition_valide(self, list_pct: list):
+        """True si tous les pourcentages sont dans les bornes min max"""
+        for i in range(len(list_pct)):
+            if list_pct[i] < self._list_min[i] or list_pct[i] > self._list_max[i]:
+                return False
+        return True
