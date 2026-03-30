65daefe47f7812f630e8cbd9e1f85966d7e06160
monjara12 <chamotsam@gmail.com>
2026-03-30 11:26:06 +0200

    Add files via upload
    distance à la droite implémenté

--- Point.py
+++ Point.py
@@ -13,3 +13,6 @@ class Point:
 
     def distance(self, autre_p : Point):
         return sqrt((-self._x + autre_p.get_x)**2 + (-self._y + autre_p.get_y)**2)
+    
+    def distance_point_droite(self, A : Point, B : Point): 
+        return abs((B.get_x() - A.get_x()) * (A.get_y() - self._y) - (A.get_x() - self._x) * (B.get_y() - A.get_x())) / A.distance(B)
