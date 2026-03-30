a55c44af025c2fbd5a436619909a05ad71e516b5
yacoub1204-ui <yacoub1204@gmail.com>
2026-03-30 17:14:09 +0200

    Add setter methods for Point
    Added setter methods for x and y coordinates.

--- Point.py
+++ Point.py
@@ -11,8 +11,13 @@ class Point:
     def get_y(self):
         return self._y
 
+    def set_x(self, x):
+        self._x=x
+    def set_y(self, y):
+        self._y=y
+
     def distance(self, autre_p : Point):
         return sqrt((-self._x + autre_p.get_x)**2 + (-self._y + autre_p.get_y)**2)
     
     def distance_point_droite(self, A : Point, B : Point): 
-        return abs((B.get_x() - A.get_x()) * (A.get_y() - self._y) - (A.get_x() - self._x) * (B.get_y() - A.get_x())) / A.distance(B)
+        return abs((B.get_x() - A.get_x()) * (A.get_y() - self._y) - (A.get_x() - self._x) * (B.get_y() - A.get_y())) / A.distance(B)
