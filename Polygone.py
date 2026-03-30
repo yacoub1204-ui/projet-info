fd54a8113826ac6bc612955b0aa896cba3d9c3e5
yacoub1204-ui <yacoub1204@gmail.com>
2026-03-30 12:23:22 +0200

    Add method to find closest point to a line

--- Polygone.py
+++ Polygone.py
@@ -6,4 +6,14 @@ class Polygone:
 
     def get_points(self):
         return self._points
-    
+        
+      def point_plus_proche_droite(self, A, B):
+        point_le_plus_proche = self._points[0]        
+        distance_minimale = distance_point_droite(A, B, self._points[0])
+        
+        for point in self._points[1:]:                 
+            d = distance_point_droite(A, B, point)
+            if d < distance_minimale:
+                distance_minimale = d
+                point_le_plus_proche = point
+        return point_le_plus_proche
