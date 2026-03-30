69359fa67323d0c1e53923c53b46dec9afbcd4bf
monjara12 <chamotsam@gmail.com>
2026-03-30 11:01:23 +0200

    Add files via upload
    figure mais on peut retourner les plaques maintenant

--- Figure.py
+++ Figure.py
@@ -1,16 +1,32 @@
-import Point
+import Polygone
 
 class Figure:
-    def __init__(self, x, y, points : list[Point]):
+
+    def __init__(self, x, y, list_p : list[Polygone]) :
         self._x = x
         self._y = y
-        self._points = points
+        self._list_p = list_p
 
     def get_x(self):
         return self._x
     
     def get_y(self):
         return self._y
-
-    def get_points(self):
-        return self._points
+    
+    def get_list_p(self):
+        return self._list_p
+    
+    def surface(self):
+        return self._x * self._y
+    
+    def tourner(self) -> None :
+        self._x ; self._y = self._y ; self._x
+        for poly in self._list_p :
+            for point in poly.get_points() :
+                point.get_x() - self._x ; point.get_y() - self._y = point.get_y() - self._y ; point.get_x() - self._x
+    
+    def decoupe_fig_tr(self):
+        solution=[]
+        for element in self._list_f() :
+            solution.append(element[0])
+        return solution
