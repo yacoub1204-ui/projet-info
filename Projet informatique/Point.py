526bd09c334b8cb38a63cda7addda862acb60f91
yacoub1204-ui <yacoub1204@gmail.com>
2026-03-30 16:55:18 +0200

    Add setters for x and y in Point class et les parenthese dans le distance jsp vu que c une fonction jme suis dis

--- Projet informatique/Point.py
+++ Projet informatique/Point.py
@@ -10,6 +10,10 @@ class Point:
 
     def get_y(self):
         return self._y
+    def set_x(self, x):
+        self._x=x
+    def set_y(self, y):
+        self._y=y
 
     def distance(self, autre_p : Point):
-        return sqrt((-self._x + autre_p.get_x)**2 + (-self._y + autre_p.get_y)**2)
+        return sqrt((-self._x + autre_p.get_x())**2 + (-self._y + autre_p.get_y())**2)
