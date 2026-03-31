db51dab6a5cfaf5e3e751082cf0c501b63eb876f
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-31 10:12:23 +0200

    Refactor Plaques class and methods

--- Plaques.py
+++ Plaques.py
@@ -43,19 +43,17 @@ class Plaques:
         return self.x<=tole_x and self._y <= tole_y
 
 
-    def tourner_plaque(self, tole_x, tole_y):
+    def tourner_plaque(self, tole.get_x(), tole.get_y()):
         """renvoi  plaque tournee ou pas"""
         nouvelle_rotation = self.ini_rotation_plaque()
-        if self.rentre_dedans(tole_x, tole_y)==False:
+        for i in range(len(list_f)):
+            if self.rentre_dedans(tole.get_x(), tole.get_y())==False:
             """ La plaque ne rentre pas: on la tourne (on met tout à 1 pour cette figure)"""
-            for i in range(len(nouvelle_rotation)):
-                for j in range(len(nouvelle_rotation[i])):
-                    nouvelle_rotation[i][j] = 1
-            return Plaque(self._y, self._x, self._dict_p, self._prix_d,
-                          self._tole, self._qte_tole, nouvelle_rotation)
-        else:
-            return Plaque(self._x, self._y, self._dict_p, self._prix_d,
-                          self._tole, self._qte_tole, nouvelle_rotation)
+                        
+                    nouvelle_rotation[i] = 1
+                return Plaque(self, self._dict_p, self._prix_d,self._list_f, nouvelle_rotation)
+            else:
+                return Plaque(self, self._dict_p, self._prix_d,self._list_f, nouvelle_rotation)
 
 
     def masse_metaux(plaques, tole : Tole, alliage):
