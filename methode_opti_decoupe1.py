fbf9666d23670b4d5f20341046a468daf66b2e61
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-31 15:04:47 +0200

    Implement def_plaque function for plaque creation
    Fix function definition and append method in def_plaque.

---
+++ methode_opti_decoupe1.py
@@ -0,0 +1,12 @@
+
+import Plaques
+import Figure
+import Tole
+
+
+
+def def_plaque(plaques.get_list_f(),figure.get_x(),figure.get_y()):
+    plaque=[]
+    for i in range(plaques.get_list_f()):
+        plaque.apppend(plaques.get_list_f[i](),figure.get_x(),figure.get_y())
+    return plaque
