0759925fdb370205cc50f1a21a0a73487259d2fb
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-31 16:50:33 +0200

    Update methode_opti_decoupe1.py

--- methode_opti_decoupe1.py
+++ methode_opti_decoupe1.py
@@ -4,11 +4,11 @@ import Tole
 
 
 
-def def_plaque(Plaques, Figures, Toles):
+def def_plaque(plaques, figures, Toles):
 """ a la fin j'aurai plaque=[("bateau",x=6,y=12),("etoile",x=9,y=3)]"""
     plaque=[]
-    for i in range(plaques.get_list_f()):
-        plaque.apppend(plaques.get_list_f[i](),figure.get_x(),figure.get_y())
+    for i in range(len(plaques.get_list_f())):
+        plaque.append(plaques.get_list_f()[i],figure.get_x(),figure.get_y())
     return plaque
 #plaque.get_list_f()[i].get_x()=x de la figure d'indice i
 
@@ -23,17 +23,17 @@ def surface_tole_restante(self, plaques,figure):
     tole_dispo=[Tole(self.x,self.y)]#liste dee morceaux de tole
     
     plaques_crees=[]
-    While plaques_restantes:
+    while plaques_restantes:
         if self.x*self.y>=figure.get_x()*figure.get_y():
             for i in range(len(toutes_plaques)):
                 if plaque.rentre_dedans(toutes_plaques[i], tole.get_x(), tole.get_y(),self.x,self.y):
                     plaques_crees.append(toutes_plaques[i])
-                    plaques_restantes.remove(plaques.get_list_f(),figure.get_x(),figure.get_y())
+                    plaques_restantes.remove((plaques.get_list_f(),figure.get_x(),figure.get_y()))
                     tole_part_bas=Tole(tole.x - figure.get_x(),figure.get_y())
                     tole_part_haut=Tole(tole.x,tole.y - figure.get_y())
                     tole=[tole_part_bas,tole_part_haut]
         toles_utilisees+=1
-        tole=(self.x,self.y)
+        tole=Tole(self.x,self.y)
 
     return toles_utilisees
                     
