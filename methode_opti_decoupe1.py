cace1604fc8e706fb1d64d9b593104eaaf32f3e1
malcomcpasdelia <malcom.perret412@gmail.com>
2026-03-31 15:35:19 +0200

    Refactor tole handling in methode_opti_decoupe1.py
    Refactor tole variable handling and update toles_utilisees.

--- methode_opti_decoupe1.py
+++ methode_opti_decoupe1.py
@@ -29,5 +29,13 @@ def surface_tole_restante(self):
                 if plaque.rentre_dedans(toutes_plaques[i], tole.get_x(), tole.get_y(),self.x,self.y):
                     plaques_crees.append(toutes_plaques[i])
                     plaques_restantes.remove(plaques.get_list_f,figure.get_x(),figure.get_y())
+                    tole_part_bas=(tole[0]-figure.get_x(),figure.get_y())
+                    tole_part_haut=(tole[0],tole[1]-figure.get_y())
+                    tole=[tole_part_bas,tole_part_haut]
+        toles_utilisees+=1
+        tole=(self.x,self.y)
+
+    return toles_utilisees, 
+                    
                     
             
