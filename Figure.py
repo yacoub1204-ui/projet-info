7189fb11db065cf99c5c58e5fe8a63443ce5b0eb
monjara12 <chamotsam@gmail.com>
2026-04-02 23:18:13 +0200

    Add files via upload

--- Figure.py
+++ Figure.py
@@ -2,10 +2,15 @@ import Polygone
 
 class Figure:
 
-    def __init__(self, x, y, list_p : list[Polygone]) :
+    def __init__(self, nom, x, y, list_poly : list[Polygone], r=0) :
+        self._nom = nom
         self._x = x
         self._y = y
-        self._list_p = list_p
+        self._list_poly = list_poly
+        self._r = r
+
+    def get_nom(self):
+        return self._nom
 
     def get_x(self):
         return self._x
@@ -13,35 +18,39 @@ class Figure:
     def get_y(self):
         return self._y
     
-    def get_list_p(self):
-        return self._list_p
+    def set_x(self, x):
+        self._x = x
+
+    def set_y(self, y):
+        self._y = y
+    
+    def get_list_poly(self):
+        return self._list_poly
+    
+    def get_r(self):
+        return self._r
+    
+    def set_r(self, r):
+        self._r = r
+
+    def __str__(self) -> str:
+        return f"{self._nom}"
     
     def surface(self):
         return self._x * self._y
     
     def tourner(self) -> None :
-        for poly in self._list_p :
+        for poly in self._list_poly :
             for point in poly.get_points() :
-                point.get_x() - self._x ; point.get_y() - self._y = point.get_y() - self._y ; point.get_x() - self._x
-        self._x ; self._y = self._y ; self._x
-    
-    def decoupe_fig_tr(self):
-        solution=[]
-        for element in self._list_f() :
-            solution.append(element[0])
-        return solution
+                x1 = point.get_x() - self._x
+                y1 = point.get_y() - self._y
 
-    def point_plus_loin(self):
-        point_max = None
-        distance_max = 0
-    
-        origine = Point(0, 0)
-    
-        for polygone in self._list_p :
-            for point in polygone.get_points():
-                d = point.distance( origine)
-                if d > distance_max:
-                    distance_max = d
-                    point_max = point
-                
-        return point_max
+                point.set_x(-y1 + self._x)
+                point.set_y(x1 + self._y)
+        
+        x = self._x
+        y = self._y
+
+        self.set_x(y)
+        self.set_y(x)
+        self.set_r((self._r +1)%2)
