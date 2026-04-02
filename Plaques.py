b74ef42486a32efdacf283c58abec2b90cb0e4ce
monjara12 <chamotsam@gmail.com>
2026-04-02 22:22:10 +0200

    Add files via upload
    plaques avec un S

--- Plaques.py
+++ Plaques.py
@@ -1,12 +1,11 @@
 import Polygone
-import Tole
-import Figure
+
 class Plaques:
 
-    def __init__(self, prix_d : float, list_f : list[Figure], rotation_plaque):
+    def __init__(self, prix_d : float, list_f : list[Figures], rotation_fig):
         self._prix_d = prix_d
         self._list_f = list_f
-        self._rotation_plaque = rotation_plaque
+        self._rotation_fig = rotation_fig
 
     def get_prix_d(self):
         """renvoie le prix du déplacement du laser"""
@@ -16,63 +15,61 @@ class Plaques:
         """renvoie la liste contenant toutes les figures à découper dans chaque plaque"""
         return self._list_f
 
-
     def get_rotation_plaque(self):
         return self._rotation_plaque
 
+    def surface(self, figure):
+        return self._x*self._y
 
+    def rentre_dedans(self):
+        return self._x <=tole_x and self._y<= tole_y
 
 
     def cout_decoupe(self):
-        return self._prix_d*figure.surface()
+        return self._prix_d*self.surface
         
 
     def ini_rotation_plaque(self):
         ini_rotation=[]
-        
-        ini_rotation.append([0 for j in range(len(list_f))])
+        for i in range(len(self._tole)):
+            nb_exemplaires = self._qte_tole[i][1]# qte_tole[i] = (nom de la figure, quantite)
+            ini_rotation.append([0] for j in range(nb_exemplaire))
         return ini_rotation
     
-    
-    def rentre_dedans(self, Figure, Tole):
+
+    def rentre_dedans(self, tole_x, tole_y):
         """renvoie booléen"""
-        return figure.get_x()<=tole.get_x() and figure.get_y() <= tole.get_y()
+        return self.x<=tole_x and self._y <= tole_y
 
 
-    def tourner_plaque(self, Tole):
+    def tourner_plaque(self, tole_x, tole_y):
         """renvoi  plaque tournee ou pas"""
         nouvelle_rotation = self.ini_rotation_plaque()
-        for i in range(len(self._list_f)):
-            if rentre_dedans(tole.get_x(), tole.get_y())==False:
+        if self.rentre_dedans(tole_x, tole_y)==False:
             """ La plaque ne rentre pas: on la tourne (on met tout à 1 pour cette figure)"""
-                        
-                nouvelle_rotation[i] = 1
-                return Plaque(self, self._dict_p, self._prix_d,self._list_f, nouvelle_rotation)
-            else:
-                return Plaque(self, self._dict_p, self._prix_d,self._list_f, nouvelle_rotation)
+            for i in range(len(nouvelle_rotation)):
+                for j in range(len(nouvelle_rotation[i])):
+                    nouvelle_rotation[i][j] = 1
+            return Plaque(self._y, self._x, self._dict_p, self._prix_d,
+                          self._tole, self._qte_tole, nouvelle_rotation)
+        else:
+            return Plaque(self._x, self._y, self._dict_p, self._prix_d,
+                          self._tole, self._qte_tole, nouvelle_rotation)
 
 
-    def masse_metaux(plaques, tole : Tole, alliage):
-        volume=sum(tole.get_x()*tole.get_y()*tole.get_z() for i in range(len(self._list_f()))"""cm^3"""
+    '''def masse_metaux(self, nb_toles, tole_x, tole_y, tole_z, alliage):
+        volume=nb_tole*tole._x*tole._y*tole.z"""cm^3"""
         mv=alliage.get_mv()"""pas sur """#en g/cm^3
         pct =alliage.get_list_pct()
         masse=[]
-        for i in range(len(self._list_f(self))):
+        for i in range(len(mv)):
             masse_metal_i=volume*mv[i]*pct[i]/1000
-            masse.append(masse_metal_i)
-        return masse
+            masse;append(masse_metal_i)
+        return masse'''
+        
 
-
-
-
-    def cout_production_tole(plaques, tole, alliage, cout_fonte):
-        cout_decoupe=get_prix_d(self)*surface(self)
-        volume=tole.get_x()*tole.get_y()*tole.get_z()"""pour une tole cm^3"""
-        cout_matiere_par_tole=alliage.get_prix_cm3()*volume
-        cout_fonte_par_tole = cout_fonte*volume
-        cout_total= nb_toles*(cout_decoupe +cout_matiere_par_tole + cout_fonte_par_tole)
- 
-        return cout_total
+    def cout_decoupe(self):
+        return self._prix_d*self.surface()
     
 
 
