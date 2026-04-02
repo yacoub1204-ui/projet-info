359319fadfe169b5f4bfe539e7118131041eb521
malcomcpasdelia <malcom.perret412@gmail.com>
2026-04-02 23:05:35 +0200

    Update methode decoupv3.py

--- methode decoupv3.py
+++ methode decoupv3.py
@@ -23,7 +23,7 @@ class Placement:
 
 class TolePlan:
     def __init__(self, tole: Tole):
-        self._tole       = tole
+        self._tole= tole
         self._placements = []   # liste des Placement(figure, x_origine, y_origine, tournee)
                                 # liste des plaques decoupees dans la tole
     def get_tole(self):
@@ -64,7 +64,7 @@ class EspaceLibre:
     def renre_dedans(self, largeur, hauteur):    
         return largeur <= self._lx and hauteur <= self._ly
     def gaspillage(self, largeur, hauteur):
-        return self.surface_libre() - largeur * hauteur   
+        return self.surface_libre() - largeur *hauteur   
 
 
 # algo decoupe
@@ -173,7 +173,7 @@ def _meilleur_placement(espaces: list[EspaceLibre], figure: Figure, autoriser_ro
             if espace.rentre_dedans(largeur, hauteur):   
                 gaspillage = espace.gaspillage(largeur, hauteur)
                 if gaspillage < meilleur_gasp:
-                    meilleur      = (indice, largeur, hauteur)
+                    meilleur= (indice, largeur, hauteur)
                     meilleur_gasp = gaspillage  
         indice += 1
 
@@ -192,14 +192,14 @@ def decouper(tole: Tole, plaques: Plaques, autoriser_rotation: bool = True):
 
         # 1. Chercher la meilleure tôle déjà utilisée
         meilleur_score = -1
-        resultat       = None
-        tole_idx       = None
-        indice_tole    = 0
+        resultat= None
+        tole_idx= None
+        indice_tole = 0
 
         for espaces in plans_espaces:
             res = _meilleur_placement(espaces, figure, autoriser_rotation)
             if res is not None:
-                surface_tole     = tole.get_x() * tole.get_y()
+                surface_tole= tole.get_x() * tole.get_y()
                 surface_utilisee = 0
                 for p in solution.get_plans()[indice_tole].get_placements():   
                     surface_utilisee += p.get_figure().get_x() * p.get_figure().get_y()    
@@ -207,8 +207,8 @@ def decouper(tole: Tole, plaques: Plaques, autoriser_rotation: bool = True):
                 score = (surface_utilisee + surface_figure) / surface_tole
                 if score > meilleur_score:
                     meilleur_score = score
-                    resultat  = res
-                    tole_idx  = indice_tole
+                    resultat= res
+                    tole_idx= indice_tole
             indice_tole += 1
 
         # 2. Si aucune tôle ne marche → essayer de fusionner
@@ -219,14 +219,14 @@ def decouper(tole: Tole, plaques: Plaques, autoriser_rotation: bool = True):
                 res = _meilleur_placement(espaces_fusionnes, figure, autoriser_rotation)
                 if res is not None:
                     plans_espaces[indice_tole] = espaces_fusionnes
-                    resultat  = res
-                    tole_idx  = indice_tole
+                    resultat= res
+                    tole_idx= indice_tole
                     break
                 indice_tole += 1
 
         # 3. Si toujours rien → créer une nouvelle tôle
         if resultat is None:
-            tole_idx       = len(solution.get_plans())    
+            tole_idx= len(solution.get_plans())    
             espace_initial = EspaceLibre(0, 0, tole.get_x(), tole.get_y())
             plans_espaces.append([espace_initial])
             solution.get_plans().append(TolePlan(tole))    
@@ -236,7 +236,7 @@ def decouper(tole: Tole, plaques: Plaques, autoriser_rotation: bool = True):
 
         # 4. Placer la figure
         indice_espace, largeur, hauteur = resultat
-        espace  = plans_espaces[tole_idx][indice_espace]
+        espace  =plans_espaces[tole_idx][indice_espace]
         tournee = (largeur != figure.get_x())
         solution.get_plans()[tole_idx].get_placements().append(   
             Placement(figure, espace.get_x(), espace.get_y(), tournee)
