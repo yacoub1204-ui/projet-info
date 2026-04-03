7139529b66ed306c08fdf4765db7b1753e976488
malcomcpasdelia <malcom.perret412@gmail.com>
2026-04-03 02:56:15 +0200

    Add files via upload

--- Plaques.py
+++ Plaques.py
@@ -1,8 +1,10 @@
+from __future__ import annotations
+import Figure
 import Polygone
 
 class Plaques:
 
-    def __init__(self, prix_d : float, list_f : list[Figures], rotation_fig):
+    def __init__(self, prix_d : float, list_f : list[Figure.Figure], rotation_fig):
         self._prix_d = prix_d
         self._list_f = list_f
         self._rotation_fig = rotation_fig
@@ -70,7 +72,3 @@ class Plaques:
 
     def cout_decoupe(self):
         return self._prix_d*self.surface()
-    
-
-
-
