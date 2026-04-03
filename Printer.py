05eff57cfbc6dd8eae647c0878afebcce4efd364
malcomcpasdelia <malcom.perret412@gmail.com>
2026-04-03 11:31:19 +0200

    Update Printer.py

--- Printer.py
+++ Printer.py
@@ -1,34 +1,36 @@
-from __future__ import annotations
 import Fonderie2
 import Plaques
 
 class Printer:
 
-    def __init__(self, fonderie : Fonderie2.Fonderie2, plaques : Plaques.Plaques):
+    def __init__(self, fonderie : Fonderie2, plaques : Plaques):
         self._fonderie = fonderie
         self._plaques = plaques
 
     def print_fournisseurs(self, fonction):
-        solution = fonction(self._fonderie, self._plaques)
+        solution = fonction(self._fonderie)
         afficher = f"{solution[0]}  {solution[1]}  {solution[2]}\n"
-        for i in range(3, len(solution)):
-            afficher += f"{solution[i]}  "
+        # solution[3] = liste des metaux achetes, solution[4] = liste des alliages utilises
+        for valeur in solution[3]:
+            afficher += f"{round(valeur, 3)}  "
+        for valeur in solution[4]:
+            afficher += f"{round(valeur, 3)}  "
         afficher += "\n"
-
         return afficher
     
     def print_plaques(self, fonction):
         solution = None
         afficher = None
 
+   # Dans Printer.py
     def print_figures(self, fonction):
-        solution = None
         afficher = ""
         for figure in self._plaques.get_list_f():
-            solution = fonction(figure)
-            afficher += f"{figure}  "
-            for i in range(len(solution)):
-                afficher += f"{solution[i]}   "
-            afficher += "\n"
-        
+            solutions = fonction(figure)  # Doit retourner une liste de solutions
+            
+            for solution in solutions:
+                afficher += f"{figure}  "
+                for valeur in solution:
+                    afficher += f"{valeur}   "
+                afficher += "\n"
         return afficher
