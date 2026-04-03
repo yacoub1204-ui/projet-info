75a7e40509955456d65d21f93298babedf2afca8
malcomcpasdelia <malcom.perret412@gmail.com>
2026-04-03 16:26:22 +0200

    Update solution_decoupe_2.py

--- solution_decoupe_2.py
+++ solution_decoupe_2.py
@@ -1,3 +1,6 @@
+
+
+
 class Placement:
     def __init__(self, figure, x_origine, y_origine, tournee):
         self._figure = figure
@@ -98,8 +101,8 @@ class EspaceLibre:
         return self.surface_libre() - largeur * hauteur  
 
 
-# algo decoupe
-def _decouper_guillotine(espace, figure, tournee):#(espace: EspaceLibre, figure: Figure, tournee: bool)
+# Algo decoupe
+def _decouper_guillotine(espace, figure, tournee):
     # decoupe un espace libre apres le placement d'un rectangle
     reste = []
     if tournee:
@@ -128,67 +131,65 @@ def _decouper_guillotine(espace, figure, tournee):#(espace: EspaceLibre, figure:
         if e.surface_libre() > 0:
             resultat.append(e)
 
-    return resultat     # liste espaces libres crees apres avoir place une figure
+    return resultat
 
 
-# def _fusionner_espaces(espaces):#(espaces: list[EspaceLibre])
-#     """Tente de fusionner des paires d'espaces dont les intervalles x ou y
-#     se chevauchent pour créer un rectangle d'espace libre qui traverse les autres."""
-#     if len(espaces) < 2:
-#         return espaces
+def _fusionner_espaces(espaces):
+    """Tente de fusionner des paires d'espaces dont les intervalles x ou y
+    se chevauchent pour créer un rectangle d'espace libre qui traverse les autres."""
+    if len(espaces) < 2:
+        return espaces
 
-#     # marquer les espaces deja fusionnes
-#     utilises = [False] * len(espaces)
-#     nouveaux = []
+    utilises = [False] * len(espaces)
+    nouveaux = []
 
-#     for i in range(len(espaces)):
-#         if utilises[i]:
-#             continue
-#         for j in range(i + 1, len(espaces)):
-#             if utilises[j]:
-#                 continue
+    for i in range(len(espaces)):
+        if utilises[i]:
+            continue
+        for j in range(i + 1, len(espaces)):
+            if utilises[j]:
+                continue
 
-#             A = espaces[i]  # rectangle coin inferieur gauche en x,y et largeur lx et hauteur ly
-#             B = espaces[j]
+            A = espaces[i]
+            B = espaces[j]
 
-#             # Fusion verticale : chevauchement en x, un rectangle avec un plus grand y est cree
-#             x_gauche = max(A.get_x(), B.get_x())
-#             x_droit  = min(A.get_x() + A.get_lx(), B.get_x() + B.get_lx())
-#             if x_droit - x_gauche > 0:
-#                 y_bas  = min(A.get_y(), B.get_y())
-#                 y_haut = max(A.get_y() + A.get_ly(), B.get_y() + B.get_ly())
-#                 fv = EspaceLibre(x_gauche, y_bas, x_droit - x_gauche, y_haut - y_bas)  # fv = fusion verticale
-#                 if fv.surface_libre() > A.surface_libre() and fv.surface_libre() > B.surface_libre():
-#                     nouveaux.append(fv)
-#                     utilises[i] = True
-#                     utilises[j] = True
-#                     break
+            # Fusion verticale : chevauchement en x
+            x_gauche = max(A.get_x(), B.get_x())
+            x_droit = min(A.get_x() + A.get_lx(), B.get_x() + B.get_lx())
+            if x_droit - x_gauche > 0:
+                y_bas = min(A.get_y(), B.get_y())
+                y_haut = max(A.get_y() + A.get_ly(), B.get_y() + B.get_ly())
+                fv = EspaceLibre(x_gauche, y_bas, x_droit - x_gauche, y_haut - y_bas)
+                if fv.surface_libre() > A.surface_libre() and fv.surface_libre() > B.surface_libre():
+                    nouveaux.append(fv)
+                    utilises[i] = True
+                    utilises[j] = True
+                    break
 
-#             # Fusion horizontale : chevauchement en y, un rectangle avec un plus grand x est cree
-#             y_bas  = max(A.get_y(), B.get_y())
-#             y_haut = min(A.get_y() + A.get_ly(), B.get_y() + B.get_ly())
-#             if y_haut - y_bas > 0:
-#                 x_gauche = min(A.get_x(), B.get_x())
-#                 x_droit  = max(A.get_x() + A.get_lx(), B.get_x() + B.get_lx())
-#                 fh = EspaceLibre(x_gauche, y_bas, x_droit - x_gauche, y_haut - y_bas)  # fh = fusion horizontale
-#                 if fh.surface_libre() > A.surface_libre() and fh.surface_libre() > B.surface_libre():
-#                     nouveaux.append(fh)
-#                     utilises[i] = True 
-#                     utilises[j] = True
-#                     break
+            # Fusion horizontale : chevauchement en y
+            y_bas = max(A.get_y(), B.get_y())
+            y_haut = min(A.get_y() + A.get_ly(), B.get_y() + B.get_ly())
+            if y_haut - y_bas > 0:
+                x_gauche = min(A.get_x(), B.get_x())
+                x_droit = max(A.get_x() + A.get_lx(), B.get_x() + B.get_lx())
+                fh = EspaceLibre(x_gauche, y_bas, x_droit - x_gauche, y_haut - y_bas)
+                if fh.surface_libre() > A.surface_libre() and fh.surface_libre() > B.surface_libre():
+                    nouveaux.append(fh)
+                    utilises[i] = True
+                    utilises[j] = True
+                    break
 
-#     if not nouveaux:
-#         return espaces
+    if not nouveaux:
+        return espaces
 
-#     result = [espaces[k] for k in range(len(espaces)) if not utilises[k]]
-#     result.extend(nouveaux)
-#     result.sort(key=lambda e: e.surface_libre(), reverse=True)  
-#     return result   # liste espaces libres combinant precedents et nouveaux (fusions)
+    result = [espaces[k] for k in range(len(espaces)) if not utilises[k]]
+    result.extend(nouveaux)
+    result.sort(key=lambda e: e.surface_libre(), reverse=True)
+    return result
 
 
-def _meilleur_placement(espaces, figure, autoriser_rotation):  # (espaces: list[EspaceLibre], figure: Figure, autoriser_rotation: bool)
-    # autoriser_rotation defini dans decouper
-    meilleur      = None
+def _meilleur_placement(espaces, figure, autoriser_rotation):
+    meilleur = None
     meilleur_gasp = float('inf')
     fx = figure.get_x()
     fy = figure.get_y()
@@ -204,34 +205,32 @@ def _meilleur_placement(espaces, figure, autoriser_rotation):  # (espaces: list[
             if espace.rentre_dedans(largeur, hauteur):   
                 gaspillage = espace.gaspillage(largeur, hauteur)
                 if gaspillage < meilleur_gasp:
-                    meilleur= (indice, largeur, hauteur)
+                    meilleur = (indice, largeur, hauteur)
                     meilleur_gasp = gaspillage  
         indice += 1
 
-    return meilleur     # tuple (indice=position dans liste espaces, largeur, hauteur apres rotation eventuelle)
+    return meilleur
 
 
-# algo principal
-def decouper(tole, plaques):#(tole: Tole, plaques: Plaques, autoriser_rotation: bool = True)
-    # return une Solution avec les TolePlan et leurs Placements
+# Algo principal
+def decouper(tole, plaques):
     autoriser_rotation = True
     figures = sorted(plaques.get_list_f(), key=lambda f: f.get_x() * f.get_y(), reverse=True)
 
-    tole_plans =[] #liste des toleplan
-    plans_espaces = []  # plans_espaces[i] = list[EspaceLibre] pour la tole i
+    tole_plans = []
+    plans_espaces = []
 
     for figure in figures:
-
-        # 1. Chercher la meilleure tôle déjà utilisée
         meilleur_score = -1
-        resultat= None
-        tole_idx= None
+        resultat = None
+        tole_idx = None
         indice_tole = 0
 
+        # 1. Chercher la meilleure tôle déjà utilisée
         for espaces in plans_espaces:
             res = _meilleur_placement(espaces, figure, autoriser_rotation)
             if res is not None:
-                surface_tole= tole.get_x() * tole.get_y()
+                surface_tole = tole.get_x() * tole.get_y()
                 surface_utilisee = 0
                 for p in tole_plans[indice_tole].get_placements():   
                     f = p.get_figure()
@@ -243,26 +242,26 @@ def decouper(tole, plaques):#(tole: Tole, plaques: Plaques, autoriser_rotation: 
                 score = (surface_utilisee + surface_figure) / surface_tole
                 if score > meilleur_score:
                     meilleur_score = score
-                    resultat= res
-                    tole_idx= indice_tole
+                    resultat = res
+                    tole_idx = indice_tole
             indice_tole += 1
 
         # 2. Si aucune tôle ne marche → essayer de fusionner
         if resultat is None:
             indice_tole = 0
             for espaces in plans_espaces:
-              #  espaces_fusionnes = _fusionner_espaces(espaces)
+                espaces_fusionnes = _fusionner_espaces(espaces)
                 res = _meilleur_placement(espaces_fusionnes, figure, autoriser_rotation)
                 if res is not None:
                     plans_espaces[indice_tole] = espaces_fusionnes
-                    resultat= res
-                    tole_idx= indice_tole
+                    resultat = res
+                    tole_idx = indice_tole
                     break
                 indice_tole += 1
 
         # 3. Si toujours rien → créer une nouvelle tôle
         if resultat is None:
-            tole_idx= len(tole_plans)    
+            tole_idx = len(tole_plans)    
             espace_initial = EspaceLibre(0, 0, tole.get_x(), tole.get_y())
             plans_espaces.append([espace_initial])
             tole_plans.append(TolePlan(tole))    
@@ -272,7 +271,7 @@ def decouper(tole, plaques):#(tole: Tole, plaques: Plaques, autoriser_rotation: 
 
         # 4. Placer la figure
         indice_espace, largeur, hauteur = resultat
-        espace  =plans_espaces[tole_idx][indice_espace]
+        espace = plans_espaces[tole_idx][indice_espace]
         tournee = (largeur != figure.get_x())
         tole_plans[tole_idx].get_placements().append(   
             Placement(figure, espace.get_x(), espace.get_y(), tournee)
@@ -285,4 +284,6 @@ def decouper(tole, plaques):#(tole: Tole, plaques: Plaques, autoriser_rotation: 
             plans_espaces[tole_idx].append(e)
         plans_espaces[tole_idx].sort(key=lambda e: e.surface_libre(), reverse=True)    
 
-    return tole_plans  # liste des TolePlan contenant tole associee a la liste des Placement (self, figure, x_origine, y_origine, tournee)
+    return tole_plans
+
+
