aff34f9dd3a491e83888312f5638fe89ead8ea91
malcomcpasdelia <malcom.perret412@gmail.com>
2026-04-03 17:20:23 +0200

    Update Alliage2.py

--- Alliage2.py
+++ Alliage2.py
@@ -1,12 +1,12 @@
 class Alliage2:
-    def __init__(self, list_pct, kg):   # CORRIGE : ordre inversé pour correspondre à reader2 Alliage2(pct, poids)
+    def __init__(self, list_pct, kg):   
         self._kg = kg
         self._list_pct = list_pct
 
     def get_kg(self):
         return self._kg
 
-    def get_list_pct(self):             # CORRIGE : manquait, utilisé dans solutions_fournisseurs
+    def get_list_pct(self):             
         return self._list_pct
 
     def get_dict_alliage(self):
